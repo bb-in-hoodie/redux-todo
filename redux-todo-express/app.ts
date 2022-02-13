093046a74db407d6c79b629c4dc11ff02a620ed1
bb-in-hoodie <pso999@naver.com>
2022-02-13 14:43:59 +0900

    feat(express): setting up TypeScript

---
+++ redux-todo-express/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Timezones by location application is running on port ${port}.`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "OK" });
+});
