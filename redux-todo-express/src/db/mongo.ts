aba305e1519ef734c22405e7e60c94bee4807ac0
bb-in-hoodie <pso999@naver.com>
2022-04-02 15:59:10 +0900

    feat(express): create a default todo list when collection is empty
    - implement initializeCollection
    - make TodoList a class
    - define names of default collection and default todo list

--- redux-todo-express/src/db/mongo.ts
+++ redux-todo-express/src/db/mongo.ts
@@ -1,4 +1,6 @@
 import { Collection, Db, MongoClient } from "mongodb";
+import { DEFAULT_COLLECTION } from "../types/collection";
+import { DEFAULT_TODO_LIST, TodoList } from "../types/todoList";
 
 let mongoClient: MongoClient | null = null;
 let reduxTodoDb: Db | null = null;
@@ -9,6 +11,7 @@ async function getClient() {
     return mongoClient;
   }
 
+  // TODO: use docker-compose internal address
   mongoClient = await MongoClient.connect(
     "mongodb://root:redux-todo-mongo@localhost:27017/"
   );
@@ -25,12 +28,30 @@ export async function closeClient() {
 }
 
 export async function getCollection() {
-  if (todoCollection) {
-    return todoCollection;
+  // if no collection is fetched yet, read one from db
+  if (!todoCollection) {
+    const client = await getClient();
+    reduxTodoDb = client.db("redux-todo");
+    todoCollection = reduxTodoDb.collection(DEFAULT_COLLECTION);
   }
 
-  const client = await getClient();
-  reduxTodoDb = client.db("redux-todo");
-  todoCollection = reduxTodoDb.collection("todo");
+  // if there is no todo list at all, create one
+  if (await todoCollection.countDocuments() < 1) {
+    await initializeCollection(todoCollection);
+  }
+
   return todoCollection;
+}
+
+async function initializeCollection(collection: Collection) {
+  // add a default todo list
+  const defaultTodoList = new TodoList(DEFAULT_TODO_LIST);
+
+  try {
+    console.log(`Creating a default todo list in collection (${collection.collectionName})`);
+    return await collection.insertOne(defaultTodoList);
+  } catch(e) {
+    console.error(`Failed to add a default todo list to collection (${collection.collectionName})`);
+    throw e;
+  }
 }
