4422c6942f67406b3d98e7ad28b5e590fac48a00
bb-in-hoodie <pso999@naver.com>
2022-04-02 16:37:53 +0900

    chore(express): add a log on cleaning up

--- redux-todo-express/src/app.ts
+++ redux-todo-express/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { closeClient, getCollection } from "./db/mongo";
+import { TodoList } from "./types/todoList";
 
 const app = express();
 const port = 3000;
@@ -8,12 +9,14 @@ app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const collection = await getCollection();
   res.json({ message: "OK" });
 });
 
 // close MongoClient when shutting down the app (https://stackoverflow.com/a/63419186/6864219)
 const cleanup = async () => {
+  console.log('Cleaning up');
   await closeClient();
   process.exit();
 }
