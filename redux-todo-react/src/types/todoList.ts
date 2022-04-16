a3ef60a51908259ee6d003fc122bc0d28b16c3b0
bb-in-hoodie <pso999@naver.com>
2022-04-16 23:00:21 +0900

    feat(react): set immerable property as true in TodoList
    - install immer
    - add immerable property to TodoList class

--- redux-todo-react/src/types/todoList.ts
+++ redux-todo-react/src/types/todoList.ts
@@ -1,6 +1,9 @@
+import { immerable } from "immer";
 import { Todo } from "./todo";
 
 export class TodoList {
+  [immerable] = true; // needed to use a class in Redux
+
   name: string;
   createdAt: Date;
   lastModifiedAt: Date;
@@ -19,5 +22,10 @@ export class TodoList {
     this.lastModifiedAt = new Date(lastModifiedAtIsoString);
     this.todos = todos;
     this.dones = dones;
+  }
+
+  static createEmptyTodoList() {
+    const now = new Date().toISOString();
+    return new TodoList("", now, now, [], []);
   }
 }
